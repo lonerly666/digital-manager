076c9de82f85dcdcaa8f3beec03005013203f7d6
Jeremy Neo <doremineo@gmail.com>
2025-03-20 20:03:32 +0800

    first push

---
+++ src/component/menu.js
@@ -0,0 +1,17 @@
+export default function Menu() {
+  return (
+    <div className="menu-container">
+      <nav className="menu">
+        <div className="menu-links">
+          <a href="/">HOME</a>
+        </div>
+        <div className="menu-links">
+          <a href="/">SETTINGS</a>
+        </div>
+        <div className="menu-links">
+          <a href="/">SUMMARY</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ src/page/home.js
@@ -0,0 +1,14 @@
+import Menu from '../component/menu';
+import '../../assets/css/home.css';
+
+export default function Home() {
+
+    return (
+        <div className="home-container">
+            <Menu/>
+            <div>
+                test
+            </div>
+        </div>
+    )
+}
